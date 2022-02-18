ef4590d3b6ba5cd42316eb0fdb5ced7893bc4bdf
TheSurgeonOfDeath <83719688+TheSurgeonOfDeath@users.noreply.github.com>
2022-02-18 11:39:53 +1030

    wrote clean csv data

--- Trading Data/read_data.m
+++ Trading Data/read_data.m
@@ -1,3 +1,8 @@
+% read data
 bitcoin = importfile("BCHAIN-MKPRU.csv");
 gold = importfile("LBMA-GOLD.csv");
 gold_clean = clean_gold_data(gold);
+
+% write data
+writetable(bitcoin, "bitcoin.csv");
+writetable(bitcoin, "gold_clean.csv");
